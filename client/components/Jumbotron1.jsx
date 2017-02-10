104700e61b9ded8d57c68e2b7bb38e3e12471cb2
Scott Chao <svinets@gmail.com>
2017-02-10 11:59:12 -0800

    Create Jumbotron jsx files

---
+++ client/components/Jumbotron1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Jumbotron1 = props => (
+  <div>
+    {/**/}
+  </div>
+);
+
+export default Jumbotron1;
---
+++ client/components/Jumbotron2.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Jumbotron2 = props => (
+  <div>
+    {/**/}
+  </div>
+);
+
+export default Jumbotron2;
