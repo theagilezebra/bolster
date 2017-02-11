35d11385a5624218173d90b3498ff0e42e64bafb
MarcManiez <marc.maniez@gmail.com>
2017-02-11 10:12:51 -0800

    Add /api/users POST route.

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const users = require('./controllers/users');
+const morgan = require('morgan');
 
 const routes = express.Router();
 
+routes.post('/users', users.createUser);
 routes.get('/users', () => {});
 
 module.exports = routes;
