f6308b5a7f904d48f7b6fb3ce56eb5b4952075c1
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-10 20:10:55 -0800

    comments out empty payload variable

--- client/actions/userActions.js
+++ client/actions/userActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export function signinOrSignup({ email, password }) {
   return dispatch => axios.post('/api/users', email, password)
     .then((response) => {
-      const payload = // define payload;
+     // const payload = // define payload;
       dispatch({ type: `SIGN${response.status === 201 ? 'IN' : 'UP'}_SUCCESSFUL`, payload });
     })
     .catch((err) => {
