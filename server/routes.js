f236d60d4738a84ba6205efc7b33d5c57d7c0576
Scott Chao <svinets@gmail.com>
2017-02-11 15:21:12 -0800

    Create route for linking to Plaid

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const plaid = require('plaid');
+const plaidClient = new plaid.Client(process.env.PLAID_CLIENT_ID, process.env.PLAID_SECRET, plaid.environments.tartan);
 
 const users = require('./controllers/users');
 const accounts = require('./controllers/accounts');
@@ -11,5 +13,24 @@ routes.post('/users/login', users.login);
 routes.put('/users/update/:email', users.update);
 
 routes.post('/accounts/create', accounts.create);
+
+routes.post('/accounts', (req, res) => {
+  plaidClient.exchangeToken(req.body.token, (exchangeErr, exchangeTokenRes) => {
+    if (exchangeErr != null) {
+      res.json(exchangeErr);
+    } else {
+      // - exchangeTokenRes.access_token (the Plaid access token - store somewhere persistent)
+      // associate the access token with a specific user and use it to request all subsequent plaid api calls
+      plaidClient.getConnectUser(exchangeTokenRes.access_token, (connectErr, connectRes) => {
+        if (connectErr != null) {
+          res.json(connectErr);
+        } else {
+          // return account data
+          res.json(connectRes);
+        }
+      });
+    }
+  });
+});
 
 module.exports = routes;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
 const middleware = require('./middleware');
 const morgan = require('morgan');
