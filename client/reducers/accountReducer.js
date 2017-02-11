b8c05af62e6d4186d771da893019153b670ba9ab
Scott Chao <svinets@gmail.com>
2017-02-11 15:23:30 -0800

    Create accountReducer.js

---
+++ client/reducers/accountReducer.js
@@ -0,0 +1,15 @@
+export default function reducer(state, action) {
+  const newState = Object.assign({}, state);
+  switch (action.type) {
+  case 'LINK_ACCOUNT_SUCCESSFUL': {
+    newState.account = action.payload;
+    break;
+  }
+  case 'LINK_ACCOUNT_FAILED': {
+    console.log('Link account failed.');
+    break;
+  }
+  // no default case
+  }
+  return newState;
+}
