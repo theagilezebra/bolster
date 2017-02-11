55e916cee17b50dda0b1759148a45a6d125b4e6b
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-10 16:38:24 -0800

    defines example budget graph component

--- client/components/TransactionChart.jsx
+++ client/components/TransactionChart.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Radar } from "react-chartjs";
 
-const TransactionChart = props => (
+const TransactionChart = (props) => (
   <div>
-    {/**/}
+    <Radar data={props.data} options={props.option} height= "450" width="900"   />
   </div>
 );
 
