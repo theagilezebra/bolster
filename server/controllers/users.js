a828575c7242d5d1ad0e4f89460ade0347a3c7ae
MarcManiez <marc.maniez@gmail.com>
2017-02-11 11:51:47 -0800

    Add createUser controller on /users POST.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,19 @@
+const User = require('../../database/models/user');
+const Address = require('../../database/models/address');
+
 module.exports = {
   createUser: (req, res) => {
-    console.log(req.body);
-    res.end('kthxbai');
+    const { firstName, lastName, email, password, address, city, state, ZIP, country } = req.body;
+    const user = { firstName, lastName, email, password };
+    const physicalAddress = { address, city, state, ZIP, country };
+    const newAddress = new Address(physicalAddress).save().then((addressInstance) => {
+      user.address_id = addressInstance.id;
+      const newUser = new User(user);
+      return newUser.save();
+    }).then((userInstance) => {
+      res.status(201).json(userInstance);
+    }).catch((err) => {
+      res.status(500).json(err);
+    });
   },
 };
