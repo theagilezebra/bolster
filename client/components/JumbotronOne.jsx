776e9ef071934ebdbb3743410d183a34c5e6268c
Scott Chao <svinets@gmail.com>
2017-02-11 17:25:30 -0800

    Change reducer reference in store to combined

--- client/components/JumbotronOne.jsx
+++ client/components/JumbotronOne.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Jumbotron }  from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 import BudgetGraph from './BudgetGraph.jsx';
-const JumbotronOne = props => ( 
+const JumbotronOne = props => (
   <div>
     <Jumbotron>
       <h1>Bonjour, je suis Jumbotron 1</h1>
-      <BudgetGraph  data= {props.data} /> 
+      <BudgetGraph data={props.data} />
     </Jumbotron>
   </div>
 );
--- client/reducers/accountReducer.js
+++ client/reducers/accountReducer.js
@@ -2,7 +2,7 @@ export default function reducer(state, action) {
   const newState = Object.assign({}, state);
   switch (action.type) {
   case 'LINK_ACCOUNT_SUCCESSFUL': {
-    newState.account = action.payload;
+    newState.accountData = action.payload;
     break;
   }
   case 'LINK_ACCOUNT_FAILED': {
