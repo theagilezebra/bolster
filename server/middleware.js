abc5f35beb1d628ebcc48f8e5f42b770d71dd485
MarcManiez <marc.maniez@gmail.com>
2017-02-11 16:02:48 -0800

    Add sessions.

--- server/middleware.js
+++ server/middleware.js
@@ -1,8 +1,16 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const helpers = require('./helpers');
 
 module.exports = (app, express) => {
   app.use(morgan('dev'));
+  app.use(session({
+    secret: 'lets make this an environment variable soon, ok?',
+    resave: false,
+    saveUninitialized: true,
+  }));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 };
