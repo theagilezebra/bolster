46949f2df319d7db63a1b6775c7f2a7c7de4423b
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-11 17:44:58 -0800

    define navbar with bootstrap components

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Signout from './Signout.jsx';
-
-const NavBar = props => ( // universal navbar renders according to user session status
-  <div>
-    {/*
-      if session exists
-        display signout button
-      else assume user is on landing page
-        display signin/signup buttons (which on click will jump down the landing page)
-    */}
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import css from '../styles/main.css';
+const LandingNav = props => ( // universal navbar renders according to user session status
+  <div >
+    <Navbar className="greennav" >
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#"> Bolster</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav bsStyle="pills" >
+        <NavItem>SignIn</NavItem>
+      </Nav>
+    </Navbar>
   </div>
 );
 
 export default connect(state => ({
   // allow signing out from any view the navbar is in, destroy the session here
-}))(NavBar);
+}))(LandingNav);
+
+
+
+/*
+if session exists
+  display signout button
+else assume user is on landing page
+  display signin/signup buttons (which on click will jump down the landing page)
+*/
