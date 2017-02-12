d5c15bd3676e346e98f0e7a2bf3bd60bdd15e907
Scott Chao <svinets@gmail.com>
2017-02-11 17:25:30 -0800

    Change reducer reference in store to combined

--- client/store.js
+++ client/store.js
@@ -1,22 +1,22 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-// import reducer from './reducers/index';
-//change this back to combined reducers, just a temporary hack to get radardata and linedata to redux
-import reducer from './reducers/userReducer.js'
+import reducer from './reducers/index';
+// change this back to combined reducers, just a temporary hack to get radardata and linedata to redux
+// import reducer from './reducers/userReducer.js';
 const middleware = applyMiddleware(logger(), thunk);
 const defaultState = {
-    radardata: {
-        labels: ["Utilities","Food And Drink","Healthcare", "Car Payment", "Mortgage"],
-        datasets: [{
-        data: [300, 180, 200, 125, 800],
-        fillColor: "#2ECC71"
-        }, 
-        {
-        data: [500, 100, 30, 100, 600],
-        fillColor: "#EFD002",
-        pointBackgroundColor: "#9B59B6"
-        }]
-      },
+  radardata: {
+    labels: ['Utilities', 'Food And Drink', 'Healthcare', 'Car Payment', 'Mortgage'],
+    datasets: [{
+      data: [300, 180, 200, 125, 800],
+      fillColor: '#2ECC71',
+    },
+    {
+      data: [500, 100, 30, 100, 600],
+      fillColor: '#EFD002',
+      pointBackgroundColor: '#9B59B6',
+    }],
+  },
 };
 export default createStore(reducer, defaultState, middleware);
