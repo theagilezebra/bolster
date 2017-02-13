4017ffaad679a7dd0a0703786513892ff3f111d0
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-13 15:41:59 -0800

    add goals component to budget graph component

--- client/components/BudgetGraph.jsx
+++ client/components/BudgetGraph.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Line } from 'react-chartjs'
-
+import Goals from './Goals.jsx'
 const BudgetGraph = props => (
   <div>
+    <Goals />
     <Line data={props.data} height="450" width="900"/>
   </div>
 );
