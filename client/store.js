c2a7090e936361814e8e40564d888b54dd4fed9d
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-13 15:41:04 -0800

    add plaid example data to store

--- client/store.js
+++ client/store.js
@@ -5,16 +5,18 @@ import reducer from './reducers/index';
 // change this back to combined reducers, just a temporary hack to get radardata and linedata to redux
 // import reducer from './reducers/userReducer.js';
 const middleware = applyMiddleware(logger(), thunk);
+const mySpending = [1200, 180, 200, 125, 800];
+const myBudget = mySpending.map((item)=>{return item * .9})
 const defaultState = {
     chartdata: {
         labels: ["Utilities","Food And Drink","Healthcare", "Car Payment", "Mortgage"],
         datasets: [
             {
-            data: [300, 180, 200, 125, 800],
+            data: mySpending,
             fillColor: "#90EE90"
             }, 
             {
-            data: [500, 100, 30, 100, 600],
+            data: myBudget,
             fillColor: "#2E8B57",
             }
         ]
