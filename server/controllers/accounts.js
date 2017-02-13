be168d85964df502e2804772f03e7671a952a1b4
Scott Chao <svinets@gmail.com>
2017-02-12 23:08:09 -0800

    Move plaid integration into accounts controller

--- server/controllers/accounts.js
+++ server/controllers/accounts.js
@@ -1,4 +1,9 @@
 const Account = require('../../database/models/account');
+const plaid = require('plaid');
+
+require('dotenv').config({ path: `${__dirname}/../../.env` });
+
+const plaidClient = new plaid.Client(process.env.PLAID_CLIENT_ID, process.env.PLAID_SECRET, plaid.environments.tartan);
 
 module.exports = {
   create: (req, res) => {
@@ -12,6 +17,23 @@ module.exports = {
     } else {
       res.status(400).end('Bad request: did you include a user_id?');
     }
+
+    // plaidClient.exchangeToken(req.body.token, (exchangeErr, exchangeTokenRes) => {
+    //   if (exchangeErr != null) {
+    //     console.log(exchangeErr);
+    //   } else {
+    //     // - exchangeTokenRes.access_token (the Plaid access token - store somewhere persistent)
+    //     // associate the access token with a specific user and use it to request all subsequent plaid api calls
+    //     plaidClient.getConnectUser(exchangeTokenRes.access_token, (connectErr, connectRes) => {
+    //       if (connectErr != null) {
+    //         console.log(connectErr);
+    //       } else {
+    //         // return account data
+    //         res.json(connectRes);
+    //       }
+    //     });
+    //   }
+    // });
   },
 
   get: (req, res) => {
