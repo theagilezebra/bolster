852c0a009b8f3f4585823cdc9d07218b2e1ff42a
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-13 15:45:22 -0800

    add example table based on chartdata in default state

--- client/components/TransactionTable.jsx
+++ client/components/TransactionTable.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
-
+import css from 'bootstrap'
 const TransactionTable = props => (
-  <div>
-    {/**/}
+  
+  <div className="transleft">
+  <div className="container">
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Category</th>
+          <th>Spent</th>
+          <th>Budget</th>
+          <th>Over/Under</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{props.data.labels[0]}</td>
+          <td>{props.data.datasets[0].data[0]}</td>
+          <td>{props.data.datasets[1].data[0]}</td>
+          <td>{props.data.datasets[0].data[0] - props.data.datasets[1].data[0] }</td>
+        </tr>
+        <tr>
+          <td>{props.data.labels[1]}</td>
+          <td>{props.data.datasets[0].data[1]}</td>
+          <td>{props.data.datasets[1].data[1]}</td>
+          <td>{props.data.datasets[0].data[1] - props.data.datasets[1].data[1] }</td>
+        </tr>
+        <tr>
+          <td>{props.data.labels[2]}</td>
+          <td>{props.data.datasets[0].data[2]}</td>
+          <td>{props.data.datasets[1].data[2]}</td>
+          <td>{props.data.datasets[0].data[2] - props.data.datasets[1].data[2] }</td>
+        </tr>
+        <tr>
+          <td>{props.data.labels[3]}</td>
+          <td>{props.data.datasets[0].data[3]}</td>
+          <td>{props.data.datasets[1].data[3]}</td>
+          <td>{props.data.datasets[0].data[3] - props.data.datasets[1].data[3] }</td>
+        </tr>
+        <tr>
+          <td>{props.data.labels[4]}</td>
+          <td>{props.data.datasets[0].data[4]}</td>
+          <td>{props.data.datasets[1].data[4]}</td>
+          <td>{props.data.datasets[0].data[4] - props.data.datasets[1].data[4] }</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
   </div>
 );
 
