30548459b22ac4016deef2b80f08958e07292c72
MarcManiez <marc.maniez@gmail.com>
2017-02-13 19:35:09 -0800

    Add findOrCreate helper.

--- server/helpers.js
+++ server/helpers.js
@@ -12,4 +12,18 @@ module.exports = {
     req.session.user = newUser.attributes;
     res.redirect('/');
   }),
+
+  findOrCreate: (model, criteria) => new Promise((resolve, reject) => {
+    model.forge(criteria).fetch().then((instance) => {
+      if (instance) {
+        resolve(instance);
+      } else {
+        model.forge(criteria).save().then((instance) => {
+          resolve(instance);
+        }).catch((err) => {
+          reject(err);
+        });
+      }
+    });
+  }),
 };
