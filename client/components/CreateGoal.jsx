714ea9d14d0d9d51166df4e4ada2f4d2f55e9c1b
Scott Chao <svinets@gmail.com>
2017-02-14 09:55:23 -0800

    Provide createGoal and dispatch to goal component

--- client/components/CreateGoal.jsx
+++ client/components/CreateGoal.jsx
@@ -1,38 +1,27 @@
 import React from 'react';
+import { createGoal } from '../actions/goalActions';
 
+export default function CreateGoal({ dispatch }) {
+  let goalName = null;
+  let goalAmount = null;
+  let goalTimeframe = null;
 
-export default function CreateGoal({dispatch}){
-    let goalName = null;
-    let goalAmount = null;
-    let goalTimeframe = null;
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(createGoal({
+      goalName: goalName.value,
+      goalAmount: goalAmount.value,
+      goalTimeframe: goalTimeframe.value,
+    }));
+  };
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        console.log(goalName.value)
-        console.log(goalAmount.value)
-        console.log(goalTimeframe.value)
-        // dispatch(createGoal({
-        //   goalName: goalName.value,
-        //   goalAmount: goalAmount.value,
-        //   goalTimeframe: goalTimeframe.value
-        // }));
-    };
-
-    return (
-      <form action="#" onSubmit={submitHandler}>
-          <h3>Create A Goal</h3>
-          <input className="inputsize" placeholder="Name your budget" ref={(ref) => { goalName = ref;}}/>
-          <input className="inputsize" placeholder="Enter amount" ref={(ref) => { goalAmount = ref;}}/>
-          <input className="inputsize" placeholder="Enter timeframe" ref={(ref) => { goalTimeframe = ref; }}/>
-          <button type="submit">Submit</button>
-      </form>
-    );
-};
-
-    {/*
-      2 options -
-      - this can be an input where users provide a budget limit, and an actual
-        goal component is created below it, or
-      - this component takes input AND transforms into the goal record itself
-    */}
-
+  return (
+    <form action="#" onSubmit={submitHandler}>
+      <h3>Create A Goal</h3>
+      <input className="inputsize" placeholder="Name your budget" ref={(ref) => { goalName = ref; }} />
+      <input className="inputsize" placeholder="Enter amount" ref={(ref) => { goalAmount = ref; }} />
+      <input className="inputsize" placeholder="Enter timeframe" ref={(ref) => { goalTimeframe = ref; }} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
