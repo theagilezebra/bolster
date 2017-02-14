935f6f8002905f74e4932399af880749fd98b739
Scott Chao <svinets@gmail.com>
2017-02-14 15:30:52 -0800

    Send post data through body

--- client/actions/categoryActions.js
+++ client/actions/categoryActions.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+export function createCategory(data) {
+  return dispatch => axios.post('/api/categories/create', data)
+    .then((response) => {
+      dispatch({ type: 'CREATE_CATEGORY_SUCCESSFUL', payload: response.data });
+    })
+    .catch((err) => {
+      dispatch({ type: 'CREATE_CATEGORY_FAILED', payload: err.response });
+    });
+}
+
 export function fetchCategories() {
   return dispatch => axios.get('/api/categories')
     .then((response) => {
