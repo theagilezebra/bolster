4de5e613f3cf88596cbf0404d18dc3e2d3e04489
Scott Chao <svinets@gmail.com>
2017-02-14 15:32:15 -0800

    Specify state for reducers

--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -1,14 +1,19 @@
-export default function reducer(state, action){
-    const newState = Object.assign({}, state);
-    switch(action.type) {
-        case 'SIGNIN_OR_SIGNUP_FAILED': {
-            console.log('SIGNIN_OR_SIGNUP_FAILED')
-            break;
-        }
-        case 'SIGNIN_OR_SIGNUP_SUCCESSFUL':{
-            console.log('SIGNIN_OR_SIGNUP_SUCCESSFUL')
-            break;
-        }
-    }
-   return newState;
+export default function reducer(state, action) {
+  const newState = Object.assign({}, state);
+  switch (action.type) {
+  case 'SIGNIN_OR_SIGNUP_SUCCESSFUL': {
+    newState.sessionActive = true;
+    break;
+  }
+  case 'SIGNIN_OR_SIGNUP_FAILED': {
+    console.log('Signin or signup failed.');
+    break;
+  }
+  case 'SIGNOUT': {
+    newState.sessionActive = false;
+    break;
+  }
+  // no default case
+  }
+  return newState;
 }
