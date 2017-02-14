bcc0503383a2bbabb0e447e34dfa9f00ce6207d7
Scott Chao <svinets@gmail.com>
2017-02-14 09:58:34 -0800

    Create routes for goals and transactions

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,7 @@ const routes = require('express').Router();
 const users = require('./controllers/users');
 const accounts = require('./controllers/accounts');
 const categories = require('./controllers/categories');
+// const goals = require('./controllers/goals');
 const transactions = require('./controllers/transactions');
 
 routes.post('/users/signup', users.create);
@@ -17,5 +18,10 @@ routes.post('/categories', categories.create);
 routes.get('/categories', categories.get);
 
 routes.get('/transactions', transactions.get);
+
+// routes.post('/budgets/create', budgets.create);
+// routes.get('/budgets', budgets.get);
+
+// routes.post('/goals/create', goals.create);
 
 module.exports = routes;
