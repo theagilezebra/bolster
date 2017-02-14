d6a2f02a02dff7f197bdb4ad164deabd447976ca
Scott Chao <svinets@gmail.com>
2017-02-14 15:29:48 -0800

    Map out extra routes

--- server/routes.js
+++ server/routes.js
@@ -3,8 +3,10 @@ const routes = require('express').Router();
 const users = require('./controllers/users');
 const accounts = require('./controllers/accounts');
 const categories = require('./controllers/categories');
-const goals = require('./controllers/goals');
+const budgets = require('./controllers/budgets');
 const transactions = require('./controllers/transactions');
+const businesses = require('./controllers/businesses');
+const goals = require('./controllers/goals');
 
 routes.post('/users/signup', users.create);
 routes.post('/users/login', users.login);
@@ -14,14 +16,19 @@ routes.post('/accounts/create', accounts.create);
 routes.get('/accounts', accounts.get);
 routes.get('/accounts/:id', accounts.getOne);
 
-routes.post('/categories', categories.create);
+routes.post('/budgets/create', budgets.create);
+routes.get('/budgets', budgets.get);
+
+routes.post('/categories/create', categories.create);
 routes.get('/categories', categories.get);
+
+routes.post('/goals/create', goals.create);
+routes.get('/goals', goals.get);
+routes.put('/goals/update/:id', goals.update);
 
 routes.get('/transactions', transactions.get);
 
-// routes.post('/budgets/create', budgets.create);
-// routes.get('/budgets', budgets.get);
+routes.get('/businesses', businesses.get);
 
-routes.post('/goals/create', goals.create);
 
 module.exports = routes;
