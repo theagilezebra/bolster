e847d87a8aa0add8898d2bd731a016c422c645a4
Scott Chao <svinets@gmail.com>
2017-02-14 09:57:46 -0800

    Combine budgerReducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,10 +3,12 @@ import users from './userReducer';
 import goals from './goalReducer';
 import transactions from './transReducer';
 import accounts from './accountReducer';
+import budgets from './budgetReducer';
 
 export default combineReducers({
-  // users,
-  // goals,
-  // transactions,
-  // accounts,
+  users,
+  goals,
+  transactions,
+  accounts,
+  budgets,
 });
