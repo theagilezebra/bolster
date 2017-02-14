5671a50bd8dd9e139099da50cc88e19b5b4cb50d
Scott Chao <svinets@gmail.com>
2017-02-14 09:59:36 -0800

    Change BudgetGraph into class component

--- client/components/BudgetGraph.jsx
+++ client/components/BudgetGraph.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Line } from 'react-chartjs'
-import Goals from './Goals.jsx'
-const BudgetGraph = props => (
-  <div>
-    <Goals />
-    <Line data={props.data} height="450" width="900"/>
-  </div>
-);
+import { Line } from 'react-chartjs';
+import Goals from './Goals.jsx';
+// import { fetchTransactions } from '../actions/transActions';
+
+class BudgetGraph extends React.Component {
+
+  componentDidMount() {
+    // this.props.dispatch(fetchTransactions()).then(() => {
+    //   console.log('helo');
+    // });
+  }
+
+  render() {
+    return (
+      <div>
+        <Goals dispatch={this.props.dispatch} />
+        <Line data={this.props.data} height="450" width="900" />
+      </div>
+    );
+  }
+}
 
 export default BudgetGraph;
