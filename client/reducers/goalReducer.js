b03fae1a064fcea24096ab94de271d45a4c7b197
Scott Chao <svinets@gmail.com>
2017-02-14 09:59:15 -0800

    Integrate state into goal and trans reducers

--- client/reducers/goalReducer.js
+++ client/reducers/goalReducer.js
@@ -1,30 +1,31 @@
-export default function reducer(state, action){
-    const newState = Object.assign({}, state);
-    switch(action.type) {
-        case 'CREATE_GOAL_SUCCESSFUL':{
-            console.log('CREATE_GOAL_SUCCESSFUL')
-            break;
-        }
-        case 'CREATE_GOAL_FAILED':{
-            console.log('CREATE_GOAL_FAILED')
-            break;
-        }
-        case 'FETCH_GOALS_SUCCESSFUL':{
-            console.log('FETCH_GOALS_SUCCESSFUL')
-            break;
-        }
-        case 'FETCH_GOALS_FAILED': {
-            console.log('FETCH_GOALS_FAILED')
-            break;
-        }
-        case 'UPDATE_GOAL_SUCCESSFUL':{
-            console.log('UPDATE_GOAL_SUCCESSFUL')
-            break;
-        }
-        case 'UPDATE_GOAL_FAILED':{
-            console.log('UPDATE_GOAL_FAILED')
-            break;
-        }
-    }
-    return newState;
+export default function reducer(state, action) {
+  const newState = Object.assign({}, state);
+  switch (action.type) {
+  case 'CREATE_GOAL_SUCCESSFUL': {
+    newState.goalData = action.payload;
+    break;
+  }
+  case 'CREATE_GOAL_FAILED': {
+    console.log('Create goal failed.');
+    break;
+  }
+  case 'FETCH_GOALS_SUCCESSFUL': {
+    console.log('FETCH_GOALS_SUCCESSFUL');
+    break;
+  }
+  case 'FETCH_GOALS_FAILED': {
+    console.log('FETCH_GOALS_FAILED');
+    break;
+  }
+  case 'UPDATE_GOAL_SUCCESSFUL': {
+    console.log('UPDATE_GOAL_SUCCESSFUL');
+    break;
+  }
+  case 'UPDATE_GOAL_FAILED': {
+    console.log('UPDATE_GOAL_FAILED');
+    break;
+  }
+  // no default
+  }
+  return newState;
 }
