4afb26262c91cbea9b36f34bf3b7518df6844cab
MarcManiez <marc.maniez@gmail.com>
2017-02-14 00:14:25 -0800

    Refactor to forge accounts.

--- server/controllers/accounts.js
+++ server/controllers/accounts.js
@@ -3,11 +3,11 @@ const Account = require('../../database/models/account');
 module.exports = {
   create: (req, res) => {
     if (req.body.user_id) {
-      new Account(req.body).save().then((account) => {
+      Account.forge(req.body).save().then((account) => {
         res.status(201).end('Account created successfully.');
       })
       .catch((err) => {
-        res.status(400).end(err);
+        res.status(400).json(err);
       });
     } else {
       res.status(400).end('Bad request: did you include a user_id?');
