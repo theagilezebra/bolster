c504451d9e9194e7716527f774f0c13ae96abc70
Scott Chao <svinets@gmail.com>
2017-02-15 10:42:02 -0800

    Remove tag from input className

--- client/components/CreateGoal.jsx
+++ client/components/CreateGoal.jsx
@@ -23,18 +23,10 @@ const CreateGoal = ({ dispatch }) => {
       <h3>Create A Goal</h3>
       <input className="inputsize inputmargin" placeholder="Name your budget" ref={(ref) => { goalName = ref; }} />
       <input className="inputsize inputmargin" placeholder="Enter amount" ref={(ref) => { goalAmount = ref; }} />
-      <input className="inputsize <inputmargin></inputmargin>" placeholder="Enter timeframe" ref={(ref) => { goalTimeframe = ref; }} />
+      <input className="inputsize inputmargin" placeholder="Enter timeframe" ref={(ref) => { goalTimeframe = ref; }} />
       <button type="submit" className="btn btn-success submitbutton">Submit</button>
     </form>
   );
 };
 
 export default connect(null)(CreateGoal);
-
-{ /*
-      2 options -
-      - this can be an input where users provide a budget limit, and an actual
-        goal component is created below it, or
-      - this component takes input AND transforms into the goal record itself
-*/ }
-
