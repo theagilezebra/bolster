10883aa0508fce089c978ff2ec40d6c930be469b
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-14 18:49:06 -0800

    style inputs and button, uncomment dispatch function in submitHandler

--- client/components/CreateGoal.jsx
+++ client/components/CreateGoal.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { createGoal } from '../actions/goalActions';
+import { connect } from 'react-redux';
+import { createGoal } from '../actions/goalActions.js';
 
 export default function CreateGoal({ dispatch }) {
+
+const CreateGoal = ({ dispatch }) => {
   let goalName = null;
   let goalAmount = null;
   let goalTimeframe = null;
@@ -16,12 +19,22 @@ export default function CreateGoal({ dispatch }) {
   };
 
   return (
-    <form action="#" onSubmit={submitHandler}>
+    <form action="#" onSubmit={submitHandler} className="centertext">
       <h3>Create A Goal</h3>
-      <input className="inputsize" placeholder="Name your budget" ref={(ref) => { goalName = ref; }} />
-      <input className="inputsize" placeholder="Enter amount" ref={(ref) => { goalAmount = ref; }} />
-      <input className="inputsize" placeholder="Enter timeframe" ref={(ref) => { goalTimeframe = ref; }} />
-      <button type="submit">Submit</button>
+      <input className="inputsize inputmargin" placeholder="Name your budget" ref={(ref) => { goalName = ref; }} />
+      <input className="inputsize inputmargin" placeholder="Enter amount" ref={(ref) => { goalAmount = ref; }} />
+      <input className="inputsize <inputmargin></inputmargin>" placeholder="Enter timeframe" ref={(ref) => { goalTimeframe = ref; }} />
+      <button type="submit" className="btn btn-success submitbutton">Submit</button>
     </form>
   );
-}
+};
+
+export default connect(null)(CreateGoal);
+
+{ /*
+      2 options -
+      - this can be an input where users provide a budget limit, and an actual
+        goal component is created below it, or
+      - this component takes input AND transforms into the goal record itself
+*/ }
+
