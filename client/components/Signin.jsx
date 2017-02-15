033dc96d8a066aec5762b0b2b5c0a7ee3a7f618a
Scott Chao <svinets@gmail.com>
2017-02-15 10:49:13 -0800

    Remove conflict tags, change FormControl to input

--- client/components/Signin.jsx
+++ client/components/Signin.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { ControlLabel, FormGroup, FormControl } from 'react-bootstrap';
+import { connect } from 'react-redux';
 import { signinOrSignup } from '../actions/userActions';
-<<<<<<< ad089a80042a4a6d8f7ea4f1a6c65fcf58af1259
-=======
-import { ControlLabel, FormGroup, FormControl } from 'react-bootstrap';
->>>>>>> correct whitespace in landing, signup, signin components
-export default function Signin({ dispatch }) {
+
+const Signin = ({ dispatch }) => {
   let emailInput = null;
   let passwordInput = null;
 
@@ -19,12 +16,11 @@ export default function Signin({ dispatch }) {
 
   return (
     <form action="#" onSubmit={submitHandler}>
-      <FormGroup>
-        <ControlLabel>Sign In </ControlLabel>
-        <FormControl className="inputsize" placeholder="Enter email" ref={(ref) => { emailInput = ref; }} />
-        <FormControl className="inputsize" placeholder="Enter super secret password" ref={(ref) => { passwordInput = ref; }} />
-        <button type="submit">Submit</button>
-      </FormGroup>
+      <input className="inputsize" placeholder="Enter email" ref={(ref) => { emailInput = ref; }} />
+      <input className="inputsize" placeholder="Enter super secret password" ref={(ref) => { passwordInput = ref; }} />
+      <button type="submit">Submit</button>
     </form>
   );
-}
+};
+
+export default connect(null)(Signin);
