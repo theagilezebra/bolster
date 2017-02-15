ad089a80042a4a6d8f7ea4f1a6c65fcf58af1259
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-14 18:54:08 -0800

    add classes to css

--- client/helpers/transactionHelpers.jsx
+++ client/helpers/transactionHelpers.jsx
@@ -1,87 +1,83 @@
 import React from 'react';
 
 const budget = {
-  'Transfer': 200,
-  'Shops': 45,
+  Transfer: 200,
+  Shops: 45,
   'Food and Drink': 450,
-  'Interest': 30
-}
+  Interest: 30,
+};
 
-const renderRows = function(mappedObj){
-  let elements = [];
-  for(let key in mappedObj) {
-    elements.push(    
-     <tr>
-      <td>{key}</td>
-      <td>{mappedObj[key]}</td>
-      <td>{budget[key]}</td>
-      <td>{budget[key] - mappedObj[key]}</td>
-    </tr>
-    )
+const renderRows = function (mappedObj) {
+  const elements = [];
+  for (const key in mappedObj) {
+    elements.push(
+      <tr>
+        <td>{key}</td>
+        <td>{mappedObj[key]}</td>
+        <td>{budget[key]}</td>
+        <td>{budget[key] - mappedObj[key]}</td>
+      </tr>,
+    );
   }
-  return elements; 
-}
+  return elements;
+};
 
-//this function does not add transactions that lack a category. it ignores.
-const mapCategories = function(data){
-  let mapped = {};
+// this function does not add transactions that lack a category. it ignores.
+const mapCategories = function (data) {
+  const mapped = {};
   data.transactions.forEach((item) => {
-    if(item.category){
-        if(mapped[item.category[0]] === undefined){
-          mapped[item.category[0]] = 0;
-        } 
-        else {
-          mapped[item.category[0]] = Math.round(mapped[item.category[0]] + item.amount);
-        }
+    if (item.category && item.category[0] != 'Transfer' && item.category[0] != 'Interest') {
+      if (mapped[item.category[0]] === undefined) {
+        mapped[item.category[0]] = 0;
+      } else {
+        mapped[item.category[0]] = Math.round(mapped[item.category[0]] + item.amount);
       }
-  })
+    }
+  });
   return mapped;
-}
+};
 
-const mapAndRender = function(data){
-  return renderRows(mapCategories(data))
-}
+const mapAndRender = function (data) {
+  return renderRows(mapCategories(data));
+};
 
-const labelize = function(data){
+const labelize = function (data) {
   const labels = [];
-    data.transactions.forEach((item) => {
-    if(item.category){
-        if(labels.includes(item.category[0]) === false){
-          console.log(item.category[0])
-          labels.push(item.category[0]);
-        } 
+  data.transactions.forEach((item) => {
+    if (item.category && item.category[0] != 'Transfer' && item.category[0] != 'Interest') {
+      if (labels.includes(item.category[0]) === false) {
+        labels.push(item.category[0]);
       }
-  })
+    }
+  });
   return labels;
-}
+};
 
-const populateChart = function(data){
+const populateChart = function (data) {
   const chartData = [];
   const chartConfig = {
-        labels: [],
-        datasets: [
-            {
-            data: [],
-            fillColor: "#90EE90"
-            },
-            {
-              //hardcoded budget. Refactor
-            data:[200, 45, 450, 30],
-            fillColor: "#2E8B57",
-            }
-         ]
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        backgroundColor: [
+          '#2E8B57',
+          '#9CF3C9',
+        ],
+      },
+    ],
   };
-  chartData.labels = labelize(data);
-  let mapped = mapCategories(data);
-  for (var key in mapped) {
-    chartData.push(mapped[key])
+  chartConfig.labels = labelize(data);
+  const mapped = mapCategories(data);
+  for (const key in mapped) {
+    chartData.push(mapped[key]);
   }
   chartConfig.datasets[0].data = chartData;
   return chartConfig;
-}
+};
 
 module.exports = {
-    budget: budget,
-    mapAndRender: mapAndRender,
-    populateChart: populateChart
-}
+  budget,
+  mapAndRender,
+  populateChart,
+};
