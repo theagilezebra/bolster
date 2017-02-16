ac455fd941f950a3465ee9a9b9102b371e3042e2
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-16 12:24:33 -0800

    add divs for line breaks for sign in /sign ou inputs

--- client/components/Signin.jsx
+++ client/components/Signin.jsx
@@ -17,8 +17,12 @@ const Signin = ({ dispatch }) => {
   return (
     <form action="#" onSubmit={submitHandler} className="quicksand">
       <h3>{'Already have an account? Sign in here!'}</h3>
-      <input className="inputsize" placeholder="Enter email" ref={(ref) => { emailInput = ref; }} />
-      <input className="inputsize" placeholder="Enter super secret password" ref={(ref) => { passwordInput = ref; }} />
+      <div>
+        <input className="inputsize inputmargin" placeholder="Enter email" ref={(ref) => { emailInput = ref; }} />
+      </div>
+      <div>
+        <input className="inputsize inputmargin" placeholder="Enter super secret password" ref={(ref) => { passwordInput = ref; }} />
+      </div>
       <button type="submit" className="btn btn-success submitbutton green">Signin</button>
     </form>
   );
--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -5,20 +5,34 @@ import { signinOrSignup } from '../actions/userActions';
 const Signup = ({ dispatch }) => {
   let emailInput = null;
   let passwordInput = null;
+  let firstNameInput = null;
+  let lastNameInput = null;
 
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(signinOrSignup({
       email: emailInput.value,
       password: passwordInput.value,
+      firstName: firstNameInput.value,
+      lastName: lastNameInput.value,
     }));
   };
 
   return (
-    <form action="#" onSubmit={submitHandler}>
+    <form action="#" onSubmit={submitHandler} className="quicksand">
       <h3>{'Don\'t have an account? Sign up to get started!'}</h3>
-      <input className="inputsize" placeholder="Enter email" ref={(ref) => { emailInput = ref; }} />
-      <input className="inputsize" placeholder="Enter super secret password" ref={(ref) => { passwordInput = ref; }} />
+      <div>
+        <input className="inputsize inputmargin" placeholder="Enter email" ref={(ref) => { emailInput = ref; }} />
+      </div>
+      <div>
+        <input className="inputsize inputmargin" placeholder="Enter super secret password" ref={(ref) => { passwordInput = ref; }} />
+      </div>
+      <div>
+        <input className="inputsize inputmargin" placeholder="Enter first name" ref={(ref) => { firstNameInput = ref; }} />
+      </div>
+      <div>
+        <input className="inputsize inputmargin" placeholder="Enter lastname" ref={(ref) => { lastNameInput = ref; }} />
+      </div>
       <button type="submit" className="btn btn-success submitbutton green">Signup</button>
     </form>
   );
