cd4a85573e0cf590e8647590a27f73a0abdfe200
Scott Chao <svinets@gmail.com>
2017-02-15 16:37:00 -0800

    Remove conflict tags from Signin

--- client/components/Signin.jsx
+++ client/components/Signin.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { signinOrSignup } from '../actions/userActions';
-<<<<<<< 07e5b2b89ec8aa6d05d3fbfd6bcf3fa0acf30393
 
 const Signin = ({ dispatch }) => {
-=======
-export default function Signin({ dispatch }) {
->>>>>>> add classes to signin/ signup component
   let emailInput = null;
   let passwordInput = null;
 
@@ -19,20 +15,10 @@ export default function Signin({ dispatch }) {
   };
 
   return (
-<<<<<<< 07e5b2b89ec8aa6d05d3fbfd6bcf3fa0acf30393
     <form action="#" onSubmit={submitHandler}>
       <input className="inputsize" placeholder="Enter email" ref={(ref) => { emailInput = ref; }} />
       <input className="inputsize" placeholder="Enter super secret password" ref={(ref) => { passwordInput = ref; }} />
       <button type="submit">Submit</button>
-=======
-    <form action="#" onSubmit={submitHandler} className="quicksand">
-      <FormGroup>
-        <ControlLabel>Sign In </ControlLabel>
-        <FormControl className="inputsize" placeholder="Enter email" ref={(ref) => { emailInput = ref; }} />
-        <FormControl className="inputsize" placeholder="Enter super secret password" ref={(ref) => { passwordInput = ref; }} />
-        <button type="submit" className="btn btn-success submitbutton green">Submit</button>
-      </FormGroup>
->>>>>>> add classes to signin/ signup component
     </form>
   );
 };
