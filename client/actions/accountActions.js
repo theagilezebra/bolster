e755e1a4a0a1d19e21258b1c94a080fc0373310b
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-17 19:40:55 -0800

    add authorization header to fetchAccoutns  get request

--- client/actions/accountActions.js
+++ client/actions/accountActions.js
@@ -11,8 +11,9 @@ export function linkAccount(data) {
     });
 }
 
-export function fetchAccounts({ userId }) {
-  return dispatch => axios.get(`/api/accounts?user_id=${userId}`)
+export function fetchAccounts(userId) {
+  const headers = { Authorization: `Bearer ${window.localStorage.userToken}` };
+  return dispatch => axios.get(`/api/accounts?user_id=${userId}`, { headers })
     .then((response) => {
       dispatch({ type: 'FETCH_ACCOUNTS_SUCCESSFUL', payload: response.data });
     })
@@ -20,3 +21,4 @@ export function fetchAccounts({ userId }) {
       dispatch({ type: 'FETCH_ACCOUNTS_FAILED', payload: err.response });
     });
 }
+
