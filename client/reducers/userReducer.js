c02ad807ff9dc7eb9c4d7022d07bdf13d0fab518
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-17 18:47:09 -0800

    store userToken in localStorage

--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -2,20 +2,21 @@ export default function reducer(state, action) {
   const newState = Object.assign({}, state);
   switch (action.type) {
 
-  case 'SIGNIN_SUCCESSFUL':
-  case 'SIGNUP_SUCCESSFUL': {
-    const { firstName, lastName, id, email } = action.payload;
-    newState.sessionActive = true;
-    newState.firstName = firstName;
-    newState.lastName = lastName;
-    newState.email = email;
-    newState.id = id;
-    break;
-  }
-  case 'SIGNOUT': {
-    newState.sessionActive = false;
-    break;
-  }
+    case 'SIGNIN_SUCCESSFUL':
+    case 'SIGNUP_SUCCESSFUL': {
+      window.localStorage.setItem('userToken', action.payload.userToken);
+      const { firstName, lastName, id, email } = action.payload.userInstance;
+      newState.sessionActive = true;
+      newState.firstName = firstName;
+      newState.lastName = lastName;
+      newState.email = email;
+      newState.id = id;
+      break;
+    }
+    case 'SIGNOUT': {
+      newState.sessionActive = false;
+      break;
+    }
   }
   return newState;
 }
