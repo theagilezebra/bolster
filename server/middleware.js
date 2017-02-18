0ce0b447c2b5c76be84e6a7c56155f82c42001c9
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-17 18:48:53 -0800

    protect routes unless specified as unprotected

--- server/middleware.js
+++ server/middleware.js
@@ -1,12 +1,15 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const expressJWT = require('express-jwt');
+const unless = require('express-unless');
 
 const helpers = require('./helpers');
+const unprotectedPaths = ['/index.html', '/', '/api/users/signup', '/api/users/signin'];
+helpers.checkUser.unless = require('express-unless');
 
 module.exports = (app, express) => {
   app.use(morgan('dev'));
-  app.use(expressJWT({ secret: process.env.JWT_SECRET || 'super secret' }).unless({ path: ['/'] }));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+  app.use('/api', helpers.checkUser.unless({ path: unprotectedPaths }));
 };
