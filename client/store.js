3cfe362e0800d7a77e7b3917bf3d36584f890438
MarcManiez <marc.maniez@gmail.com>
2017-02-18 12:02:53 -0800

    Reflect file name change for default state.

--- client/store.js
+++ client/store.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers/index';
 
-import defaultState from './helpers/defaultState';
+import defaultState from './helpers/stateHelpers';
 
 const middleware = applyMiddleware(logger(), thunk);
 
