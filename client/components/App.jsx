7fdcf2d014decd192f2e87c18de3201eb62061d3
MarcManiez <marc.maniez@gmail.com>
2017-02-18 18:22:26 -0800

    Check user auth and load user data on page load.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Router, Route, hashHistory } from 'react-router';
-import requireAuth from '../helpers/authHelpers';
 
+import { fetchAccounts } from '../actions/accountActions';
+import fetchTransactions from '../actions/transActions';
+import { fetchGoals } from '../actions/goalActions';
+import { fetchBudgets } from '../actions/budgetActions';
+import { checkAuth } from '../actions/userActions';
+import requireAuth from '../helpers/authHelpers';
 // actions and other components
 import Landing from './Landing.jsx';
 import Dashboard from './Dashboard.jsx';
@@ -10,6 +15,20 @@ import BudgetGraph from './BudgetGraph.jsx';
 import Transactions from './Transactions.jsx';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    const dispatch = this.props.dispatch;
+
+    dispatch(checkAuth())
+    .then(() => dispatch(fetchAccounts()))
+    .then(() => dispatch(fetchTransactions()))
+    .then(() => dispatch(fetchGoals()))
+    .then(() => dispatch(fetchBudgets()))
+    .catch(() => {
+      console.err('uninformative error: fetching data');
+    });
+  }
+
   render() {
     return (
       <Router history={hashHistory}>
