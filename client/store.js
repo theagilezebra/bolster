2d033128793eaefad17e218a9ef896c64b8363b1
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-20 14:41:57 -0800

    add goals to store

--- client/store.js
+++ client/store.js
@@ -6,5 +6,32 @@ import reducer from './reducers/index';
 import { defaultState } from './helpers/stateHelpers';
 
 const middleware = applyMiddleware(logger(), thunk);
+const defaultState = {
+  render: {
+    signForm: 'Signup',
+  },
+
+  goals: {
+    user_id: null,
+    date: null,
+    name: null,
+    amount: null,
+  },
+
+  user: {
+    sessionActive: null,
+    id: 1,
+  },
+  error: {
+    signup: '',
+    signin: '',
+  },
+  accounts: {
+    accountData: [],
+  },
+  transactions: {
+    transactionsData: [],
+  },
+};
 
 export default createStore(reducer, defaultState, middleware);
