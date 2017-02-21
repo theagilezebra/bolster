280d7bc083ff566d2c8bc7bec39516aa078d9693
Scott Chao <svinets@gmail.com>
2017-02-21 15:31:15 -0800

    Remove accounts and transactions from AddBank

--- client/components/AddBank.jsx
+++ client/components/AddBank.jsx
@@ -2,16 +2,10 @@ import React from 'react';
 import PlaidLink from 'react-plaid-link';
 import { connect } from 'react-redux';
 import { linkAccounts } from '../actions/accountActions';
-import { requestTransactions, fetchTransactions } from '../actions/transActions';
 
-const AddBank = ({ id, accounts, transactions, dispatch, error }) => {
-  const { accountData, accountStatus } = accounts;
+const AddBank = ({ dispatch, error }) => {
   const handleOnSuccess = (public_token, metadata) => {
     dispatch(linkAccounts({ public_token, institutionName: metadata.institution.name }));
-  };
-
-  const renderTransactions = () => {
-    dispatch(requestTransactions()).then(() => dispatch(fetchTransactions()));
   };
 
   return (
@@ -23,29 +17,11 @@ const AddBank = ({ id, accounts, transactions, dispatch, error }) => {
         clientName="Bolster"
         onSuccess={handleOnSuccess}
       />
-      <div>
-         {accounts.map(account => (
-          <div>
-            <span>Account: {account.name} ..... Balance: {account.currentBalance}</span>
-          </div>
-        ))}
-       </div>
-      <div>
-        {transactions.map(transaction => (
-          <div>
-            <span>Transaction: {transaction.name} ..... Amount: {transaction.amount}</span>
-          </div>
-        ))}
-      </div>
       <p className="error-message">{error}</p>
-      <button onClick={() => { renderTransactions(); }}>GET TRANSACTIONS</button>
     </div>
   );
 };
 
 export default connect(state => ({
-  accounts: state.accounts.accountData,
-  id: state.user.id,
-  error: state.error.addBank,
-  transactions: state.transactions.transactionsData,
+  error: state.error.link,
 }))(AddBank);
