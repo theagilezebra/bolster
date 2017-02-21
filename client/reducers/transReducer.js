afc0d79c4261d84c3bc6610640f939a572c91935
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-20 17:02:14 -0800

    remove signout cases in reducers

--- client/reducers/transReducer.js
+++ client/reducers/transReducer.js
@@ -4,26 +4,22 @@ import { resetState } from '../helpers/stateHelpers';
 export default function (state, action) {
   const newState = Object.assign({}, state);
   switch (action.type) {
-  case 'REQUEST_TRANSACTIONS_SUCCESSFUL': {
-    newState.transactionsData = action.payload;
-    break;
-  }
-  case 'REQUEST_TRANSACTIONS_FAILED': {
-    console.log('Request transactions failed.');
-    break;
-  }
-  case 'FETCH_TRANSACTIONS_SUCCESSFUL': {
-    newState.transactionsData = convertTransactions(action.payload);
-    break;
-  }
-  case 'FETCH_TRANSACTIONS_FAILED': {
-    console.log('FETCH_TRANSACTIONS_FAILED');
-    break;
-  }
-  case 'SIGNOUT': {
-    resetState(newState, 'transactions');
-    break;
-  }
+    case 'REQUEST_TRANSACTIONS_SUCCESSFUL': {
+      newState.transactionsData = action.payload;
+      break;
+    }
+    case 'REQUEST_TRANSACTIONS_FAILED': {
+      console.log('Request transactions failed.');
+      break;
+    }
+    case 'FETCH_TRANSACTIONS_SUCCESSFUL': {
+      newState.transactionsData = convertTransactions(action.payload);
+      break;
+    }
+    case 'FETCH_TRANSACTIONS_FAILED': {
+      console.log('FETCH_TRANSACTIONS_FAILED');
+      break;
+    }
   }
   return newState;
 }
