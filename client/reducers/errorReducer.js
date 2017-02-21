47c9953093c496c617f7b06197030cde52de1ecc
Scott Chao <svinets@gmail.com>
2017-02-21 11:42:33 -0800

    Remove resetState from reducers

--- client/reducers/errorReducer.js
+++ client/reducers/errorReducer.js
@@ -1,20 +1,18 @@
-import { resetState } from '../helpers/stateHelpers';
-
 export default function reducer(state, action) {
   const newState = Object.assign({}, state);
   switch (action.type) {
-    case 'SIGNUP_FAILED': {
-      newState.signup = action.payload;
-      break;
-    }
-    case 'SIGNIN_FAILED': {
-      newState.signin = action.payload;
-      break;
-    }
-    case 'LINK_ACCOUNTS_FAILED': {
-      newState.addBank = action.payload;
-      break;
-    }
+  case 'SIGNUP_FAILED': {
+    newState.signup = action.payload;
+    break;
+  }
+  case 'SIGNIN_FAILED': {
+    newState.signin = action.payload;
+    break;
+  }
+  case 'LINK_ACCOUNTS_FAILED': {
+    newState.addBank = action.payload;
+    break;
+  }
   }
   return newState;
 }
