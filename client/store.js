7acf5958246d37297d09f29339bbb0b3fee76d02
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-20 17:05:14 -0800

    remove default state from store

--- client/store.js
+++ client/store.js
@@ -2,36 +2,8 @@ import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers/index';
-
 import { defaultState } from './helpers/stateHelpers';
 
 const middleware = applyMiddleware(logger(), thunk);
-const defaultState = {
-  render: {
-    signForm: 'Signup',
-  },
-
-  goals: {
-    user_id: null,
-    date: null,
-    name: null,
-    amount: null,
-  },
-
-  user: {
-    sessionActive: null,
-    id: 1,
-  },
-  error: {
-    signup: '',
-    signin: '',
-  },
-  accounts: {
-    accountData: [],
-  },
-  transactions: {
-    transactionsData: [],
-  },
-};
 
 export default createStore(reducer, defaultState, middleware);
