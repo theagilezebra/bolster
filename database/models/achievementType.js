8d12187387591c15b6c2e92269c29772658ee756
MarcManiez <marc.maniez@gmail.com>
2017-02-22 22:11:27 -0800

    Add an achievement to every user's account when a an achievement type is created.

--- database/models/achievementType.js
+++ database/models/achievementType.js
@@ -1,8 +1,21 @@
 const db = require('../database');
-const Achievement = require('./achievement');
+const User = require('./user.js');
+const Achievement = require('./achievement.js');
 
 module.exports = db.Model.extend({
-  tableName: 'achievementTypes',
+  tableName: 'achievementtypes',
   hasTimestamps: true,
   achievements: () => this.hasMany(Achievement),
+  initialize() {
+    this.on('created', (achievementType) => {
+      require('./user').fetchAll()
+      .then((users) => Promise.all(users.models.map(user => require('./achievement.js').forge({
+        user_id: user.id,
+        achievementtypes_id: achievementType.id,
+      }).save())))
+      .catch((err) => {
+        console.log('ERROR', err);
+      });
+    });
+  },
 });
