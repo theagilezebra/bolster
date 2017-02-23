93ac384b07c487ac163cf0ff854ab87980fba7ff
MarcManiez <marc.maniez@gmail.com>
2017-02-22 16:17:15 -0800

    Reflect new user relationship with achievements.

--- database/models/user.js
+++ database/models/user.js
@@ -3,6 +3,7 @@ const Account = require('./account');
 const Address = require('./address');
 const Transaction = require('./transaction');
 const Goal = require('./goal');
+const Achievement = require('./achievement');
 
 module.exports = db.Model.extend({
   tableName: 'users',
@@ -11,4 +12,5 @@ module.exports = db.Model.extend({
   address: () => this.belongsTo(Address),
   transactions: () => this.hasMany(Transaction),
   goals: () => this.hasMany(Goal),
+  achievements: () => this.hasMany(Achievement),
 });
