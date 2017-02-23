e0987a9e3a5d4d4673619ce4d3f7b2ffc4f278eb
MarcManiez <marc.maniez@gmail.com>
2017-02-23 12:48:35 -0800

    Build custom delete method.

--- database/models/achievementType.js
+++ database/models/achievementType.js
@@ -9,12 +9,27 @@ module.exports = db.Model.extend({
   initialize() {
     this.on('created', (achievementType) => {
       require('./user').fetchAll()
-      .then((users) => Promise.all(users.models.map(user => require('./achievement.js').forge({
+      .then(users => Promise.all(users.models.map(user => require('./achievement.js').forge({
         user_id: user.id,
         achievementtypes_id: achievementType.id,
       }).save())))
       .catch((err) => {
         console.log('ERROR', err);
+      });
+    });
+  },
+
+  delete() {
+    return new Promise((resolve, reject) => {
+      console.log('THIS INSIDE ARROW FUNCTION, YAY FOR ARROW FUNCTIONS!?!?', this);
+      require('./achievement.js').forge().where({ achievementtypes_id: this.id }).fetchAll()
+      .then(achievements => Promise.all(achievements.models.map(achievement => achievement.destroy())))
+      .then(() => this.destroy())
+      .then((destroyed) => {
+        resolve(destroyed);
+      })
+      .catch((err) => {
+        reject(err);
       });
     });
   },
