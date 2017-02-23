4e216c1c01ff965ecba5b0033d2080b79bc9adb5
MarcManiez <marc.maniez@gmail.com>
2017-02-22 18:12:39 -0800

    Create list of achievement types.

---
+++ database/achievementTypes.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: 'Eat 5 burritos in a day',
+    structure: 'something',
+    description: 'you\'re not saving money this way!',
+  },
+  {
+    name: 'spend on a 24 carat diamond',
+    structure: '',
+    description: 'not sure that\'s good for your wallet buddy!',
+  },
+  {
+    name: 'buy a third TV screen',
+    structure: '',
+    description: 'that had better not been a consumer load!',
+  },
+];
