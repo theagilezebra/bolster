bddbf45ab7ce462398faffb4e83126ce3c223086
MarcManiez <marc.maniez@gmail.com>
2017-02-22 16:17:30 -0800

    Add new models and collections to indices.

--- database/collections/index.js
+++ database/collections/index.js
@@ -2,15 +2,19 @@ const db = require('../database');
 
 const Accounts = require('./accounts');
 const Addresses = require('./addresses');
+const Achievements = require('./achievements');
+const AchievementTypes = require('./achievementTypes');
 const Budgets = require('./budgets');
-const Users = require('./users');
 const Businesses = require('./businesses');
 const Categories = require('./categories');
 const Goals = require('./goals');
 const Transactions = require('./transactions');
+const Users = require('./users');
 
 module.exports = {
   Accounts,
+  Achievements,
+  AchievementTypes,
   Addresses,
   Budgets,
   Businesses,
--- database/models/index.js
+++ database/models/index.js
@@ -1,16 +1,20 @@
 const db = require('../database');
 
 const Account = require('./account');
+const Achievement = require('./achievement');
+const AchievementTypes = require('./achievement');
 const Address = require('./address');
 const Budget = require('./budget');
-const User = require('./user');
 const Business = require('./business');
 const Category = require('./category');
 const Goal = require('./goal');
 const Transaction = require('./transaction');
+const User = require('./user');
 
 module.exports = {
   Account,
+  Achievement,
+  AchievementTypes,
   Address,
   Budget,
   Business,
