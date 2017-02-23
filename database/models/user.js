a54e8264842871bd50ebfefe80516c710d3b2bbb
MarcManiez <marc.maniez@gmail.com>
2017-02-22 19:23:43 -0800

    Create one achivement of each achievement type on user creation.

--- database/models/user.js
+++ database/models/user.js
@@ -4,6 +4,7 @@ const Address = require('./address');
 const Transaction = require('./transaction');
 const Goal = require('./goal');
 const Achievement = require('./achievement');
+const AchievementType = require('./achievementType');
 
 module.exports = db.Model.extend({
   tableName: 'users',
@@ -13,4 +14,13 @@ module.exports = db.Model.extend({
   transactions: () => this.hasMany(Transaction),
   goals: () => this.hasMany(Goal),
   achievements: () => this.hasMany(Achievement),
+  initialize() {
+    this.on('created', (user) => {
+      AchievementType.forge().fetchAll()
+      .then(achievementTypes => Promise.all(achievementTypes.models.map(achievementType => Achievement.forge({
+        user_id: user.id,
+        achievementtypes_id: achievementType.id,
+      }).save())));
+    });
+  },
 });
