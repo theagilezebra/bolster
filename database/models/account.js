bd37611bc775df1ec2bf0f896329fb27c4374a5f
MarcManiez <marc.maniez@gmail.com>
2017-02-23 19:36:56 -0800

    Validate account registration achievement on connect.

--- database/models/account.js
+++ database/models/account.js
@@ -1,10 +1,30 @@
 const db = require('../database');
 const User = require('./user');
 const Transaction = require('./transaction');
+const Achievement = require('./achievement');
+const AchievementType = require('./achievementType');
 
 module.exports = db.Model.extend({
   tableName: 'accounts',
   hasTimestamps: true,
   transactions: () => this.hasMany(Transaction),
   user: () => this.belongsTo(User),
+  initialize() {
+    this.on('created', (account) => {
+      AchievementType.forge({ name: 'Making bank' }).fetch()
+      .then(achievementType => Achievement.forge().where({
+        achievementtypes_id: achievementType.id,
+        user_id: account.attributes.user_id,
+      }).fetch())
+      .then((achievement) => {
+        if (!achievement.attributes.status) {
+          achievement.attributes.status = true;
+          achievement.save();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    });
+  },
 });
