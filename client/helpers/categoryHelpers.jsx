f8385c1ab42c4276c2325dcaf0af453aa634ce7a
Scott Chao <svinets@gmail.com>
2017-02-24 08:46:27 -0800

    Sort subcategories alphabetically in store

--- client/helpers/categoryHelpers.jsx
+++ client/helpers/categoryHelpers.jsx
@@ -1,13 +1,17 @@
 export default function (categoryList) {
-  return categoryList.map((category) => {
+  const categoryObj = categoryList.map((category) => {
     category.categories = JSON.parse(category.categories);
     return category;
   })
   .filter(({ categories }) => categories.length === 2)
   .reduce((obj, { categories }) => {
-    !obj[categories[0]]
-      ? obj[categories[0]] = []
-      : obj[categories[0]].push(categories[1]);
+    !obj[categories[0]] ? obj[categories[0]] = [] : obj[categories[0]].push(categories[1]);
     return obj;
   }, {});
+
+  for (const category in categoryObj) {
+    categoryObj[category] = categoryObj[category].sort();
+  }
+
+  return categoryObj;
 }
