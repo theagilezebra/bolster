e16f9ef1c300d04ffc6960402c167af40ca1fc35
MarcManiez <marc.maniez@gmail.com>
2017-02-24 20:24:42 -0800

    Add address info to user state.

--- client/helpers/userHelpers.js
+++ client/helpers/userHelpers.js
@@ -1,9 +1,14 @@
 export default function decorateState(newState, payload) {
-  const { firstName, lastName, id, email } = payload;
+  const { firstName, lastName, id, email, address, city, state, zip, country } = payload;
   newState.sessionActive = true;
   newState.firstName = firstName;
   newState.lastName = lastName;
+  newState.address = address;
+  newState.country = country;
   newState.email = email;
+  newState.state = state;
+  newState.city = city;
+  newState.zip = zip;
   newState.id = id;
   return newState;
 }
