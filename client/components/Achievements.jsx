4021896a3eb94b507581af6c6d7f028e237762d1
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-24 19:08:43 -0800

    add games to test progress bar rendering with sample data

--- client/components/Achievements.jsx
+++ client/components/Achievements.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Game from './Game.jsx';
 import NavigationBar from './NavBar.jsx';
+import Stats from './Stats.jsx';
 import { getPurchases, progressBar, average, previousMonth } from '../helpers/gameHelpers.jsx';
 const Achievements = props => (
   <div>
     <NavigationBar />
-    <div className="container-fluid">
+    <div className="container quicksand">
       <div className="row">
-        <Game name="Daily Challenge" start="2015-10-20" days={`${1}`} period="daily" />
-        <Game name="Weekly Challenge" start="2015-10-20" days={`${7}`} period="weekly" />
+        <Game name="Trappist Monk" start="2017-01-29" days={`${1}`} period="daily" />
+        <Game name="Daily Hero" start="2015-10-20" days={`${7}`} period="daily" />
+        <Game name="Weekly Hero" start="2015-10-20" days={`${7}`} period="weekly" />
       </div>
+    </div>
+    <div >
+      <h1 className="quicksand centertext" >Past Achievements</h1>
+      <Stats />
     </div>
   </div>
   );
