856fdb0160084ee653d55abe8cb62aad02041f7e
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-27 15:19:29 -0800

    add the add bank component to the institutions component

--- client/components/Institutions.jsx
+++ client/components/Institutions.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { renderAccountGroups, groupAccounts } from '../helpers/accountHelpers.jsx';
+import AddBank from './AddBank.jsx';
 
 const Institutions = ({ accounts }) => (
   <div>
     <h2>Accounts</h2>
     {renderAccountGroups(groupAccounts(accounts))}
+    <h3>Add a bank</h3>
+    <AddBank />
   </div>
+
 );
 
 export default Institutions;
