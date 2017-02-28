da01c85e2fd1caa0b706c10b16cfba602c45955a
Scott Chao <svinets@gmail.com>
2017-02-28 14:05:49 -0800

    Send email to user upon receiving achievement

--- database/models/achievement.js
+++ database/models/achievement.js
@@ -1,10 +1,49 @@
+const nodemailer = require('nodemailer');
 const db = require('../database');
-const AchievementTypes = require('./achievementType');
+const AchievementType = require('./achievementType');
 const User = require('./user');
+
+require('dotenv').config({ path: `${__dirname}/../../.env` });
 
 module.exports = db.Model.extend({
   tableName: 'achievements',
   hasTimestamps: true,
-  achievementType: () => this.belongsTo(AchievementTypes),
+  achievementType: () => this.belongsTo(AchievementType),
   user: () => this.belongsTo(User),
+  initialize() {
+    this.on('updated', ({ attributes }) => {
+      let name;
+      let email;
+      let achievementName;
+      let description;
+
+      require('./user').where({ id: attributes.user_id }).fetch()
+      .then((user) => {
+        name = user.attributes.firstName;
+        email = user.attributes.email;
+      })
+      .then(() => AchievementType.where({ id: attributes.achievementtypes_id }).fetch()
+      .then((achievementtype) => {
+        achievementName = achievementtype.attributes.name;
+        description = achievementtype.attributes.description;
+      }))
+      .then(() => nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'bolsteryourbudget@gmail.com',
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      })
+      .sendMail({
+        from: '"Bolster" <bolsteryourbudget@gmail.com>',
+        to: `${email}`,
+        subject: 'Achievement unlocked!',
+        text: `Nice job ${name}!, You've earned an achievement!`,
+        html: `Nice job ${name}!, You've earned the achievement<h1>${achievementName}</h1>` +
+              `for completing the following requirement --<br><b>${description}<b>`,
+      }, (err, info) => err
+        ? console.log(err)
+        : console.log('Message %s sent: %s', info.messageId, info.response)));
+    });
+  },
 });
