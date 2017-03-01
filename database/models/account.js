a5738094a3da19dcb7b63dbf3b8c6bb378b6fcbe
MarcManiez <marc.maniez@gmail.com>
2017-03-01 15:35:19 -0800

    Remove initialize hook and created listener.

--- database/models/account.js
+++ database/models/account.js
@@ -9,22 +9,4 @@ module.exports = db.Model.extend({
   hasTimestamps: true,
   transactions: () => this.hasMany(Transaction),
   user: () => this.belongsTo(User),
-  initialize() {
-    this.on('created', (account) => {
-      AchievementType.forge({ name: 'Making bank' }).fetch()
-      .then(achievementType => Achievement.forge().where({
-        achievementtypes_id: achievementType.id,
-        user_id: account.attributes.user_id,
-      }).fetch())
-      .then((achievement) => {
-        if (!achievement.attributes.status) {
-          achievement.attributes.status = true;
-          achievement.save();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    });
-  },
 });
