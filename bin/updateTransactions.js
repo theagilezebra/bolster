cccd89a15223c28befadef30797879fc34c0bdc3
colbisaurusrex <c.barthelmess1@gmail.com>
2017-02-28 19:30:59 -0800

    compute achievements using Promise.all

--- bin/updateTransactions.js
+++ bin/updateTransactions.js
@@ -1,9 +1,9 @@
 // #! <node path inside of the heroku instance, find by using heroku run bash and which node>
 const User = require('../database/models/user');
+const Achievement = require('../database/models/achievement');
 const AchievementType = require('../database/models/achievementType');
 const transactions = require('../server/controllers/transactions');
 const plaid = require('../server/controllers/plaid');
-const gameHelpers = require('../server/AchievementsService/gameHelpers');
 const achievementCalculator = require('../server/AchievementsService/achievementCalculators');
 
 function updateTransactions() {
@@ -16,21 +16,22 @@ function updateTransactions() {
   .then(users => Promise.all(users.models.map(user => plaid.transactions.get(user.id)
     .then(results => results.data.transactions)
     .then(purchases => transactions.bulkCreate(purchases, user.id))
-    .then((purchases) => {
-      // TODO: there is an issue with the transactions: some purchases come back undefined, and we get too many (12) compared to what is in the DB (9)
-      // const config = { creationDate: user.attributes.created_at, category: '13005000', purchases };
-      // console.log(gameHelpers.periodicAchievementGenerator(config));
-      for (let i = 0; i < achievements.length; i += 1) {
-        console.log('----------', achievements);
-        if (achievements[i].attributes.structure !== 'profile') {
-          achievementCalculator[achievements[i].attributes.name](purchases);
-        }
-        // update transaction
+    .then(purchases => Promise.all(achievements.map((achievement) => {
+      if (achievement.attributes.structure !== 'profile') {
+        const calculation = achievementCalculator[achievement.attributes.name](purchases);
+        return Achievement.forge({ user_id: user.id, achievementtypes_id: achievement.id }).fetch()
+        .then((achievementInstance) => {
+          if (!achievementInstance.attributes.status) {
+            return achievementInstance.save(calculation);
+          }
+        });
       }
-    }))))
+    }))))))
   .catch((err) => {
     console.log(err);
   });
 }
 
 updateTransactions();
+
+ // TODO: there is an issue with the transactions: some purchases come back undefined, and we get too many (12) compared to what is in the DB (9)
