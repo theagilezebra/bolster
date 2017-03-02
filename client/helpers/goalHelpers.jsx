7eb1d2e0adc6c01998149cc13f1bfbc3f7ff0950
Scott Chao <svinets@gmail.com>
2017-03-01 20:00:22 -0800

    Create function to parse human readable date

--- client/helpers/goalHelpers.jsx
+++ client/helpers/goalHelpers.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import moment from 'moment';
-const displayGoals = (goals, handler) => {
+
+const displayGoals = (goals, callback, handler) => {
   const elements = [];
   goals.forEach((goal) => {
     elements.push(
-      <tr>
+      <tr onClick={callback}>
         <td>{goal.name}</td>
         <td>{goal.amount}</td>
-        <td>{moment(goal.date).format('MMMM Do YYYY')}</td>
+        <td>{moment(goal.startDate).format('MMMM Do YYYY')}</td>
+        <td>{moment(goal.endDate).format('MMMM Do YYYY')}</td>
         <td><button data-goal={goal.id} onClick={handler}> Delete Goal</button></td>
       </tr>,
     );
@@ -15,6 +17,27 @@ const displayGoals = (goals, handler) => {
   return elements;
 };
 
+const parseDate = (date) => {
+  date = date.split(' ');
+  const months = {
+    January: '01',
+    February: '02',
+    March: '03',
+    April: '04',
+    May: '05',
+    June: '06',
+    July: '07',
+    August: '08',
+    September: '09',
+    October: '10',
+    November: '11',
+    December: '12',
+  };
+  const day = date[1].replace(/[dhnrst]/g, '');
+  return `${date[2]}-${months[date[0]]}-${day.length !== 1 ? day : 0 + day}`;
+}
+
 module.exports = {
   displayGoals,
+  parseDate,
 };
