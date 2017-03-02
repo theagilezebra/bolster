8402d96cd21bd5f4e37ac6c4d072a7d2d8f0882b
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-01 21:05:53 -0800

    define deleteHandler, pass it as parameter to displayGoals function

--- client/components/CreateGoal.jsx
+++ client/components/CreateGoal.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createGoal, fetchGoals } from '../actions/goalActions.js';
+import { createGoal, fetchGoals, deleteGoal } from '../actions/goalActions.js';
 import { displayGoals } from '../helpers/goalHelpers.jsx';
 
 const CreateGoal = (props) => {
   let goalName = null;
   let goalAmount = null;
   let goalTimeframe = null;
+  let goalID = null;
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -15,6 +16,18 @@ const CreateGoal = (props) => {
       name: goalName.value,
       amount: goalAmount.value,
       date: goalTimeframe.value,
+    }));
+    goalName.value = '';
+    goalAmount.value = '';
+    goalTimeframe.value = '';
+  };
+
+  const deleteHandler = (e) => {
+    e.preventDefault();
+    goalID = Number(e.target.attributes['data-goal'].value);
+    props.dispatch(deleteGoal({
+      user_id: props.user_id,
+      goal_id: goalID,
     }));
   };
 
@@ -38,7 +51,7 @@ const CreateGoal = (props) => {
             </tr>
           </thead>
           <tbody>
-            {displayGoals(props.goals)}
+            {displayGoals(props.goals, deleteHandler)}
           </tbody>
         </table>
       </div>
