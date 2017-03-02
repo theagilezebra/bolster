c5ce6444fb2cf68553fb9f378a19dafc31263d22
MarcManiez <marc.maniez@gmail.com>
2017-03-02 12:26:09 -0800

    Resolve TODOs.

--- server/routes.js
+++ server/routes.js
@@ -17,23 +17,22 @@ routes.put('/users/:id', users.update);
 routes.get('/users/auth', users.checkAuth);
 routes.delete('/users', users.delete);
 
-// routes.post('/accounts/create', accounts.create); // --WE DO NOT USE THIS ENDPOINT---TODO: make this into '/accounts'. 'create' is implicit thanks to POST method
 routes.get('/accounts', accounts.get);
 routes.get('/accounts/:id', accounts.getOne); // ----NO NEED--- TODO: refactor this functionality into /accounts with query parameters
 
-routes.post('/budgets', budgets.create); // --DONE--- TODO: make this into '/budgets'. 'create' is implicit thanks to POST method
+routes.post('/budgets', budgets.create);
 routes.get('/budgets', budgets.get);
 
 routes.post('/categories/create', categories.create); // TODO: make this into '/categories'. 'create' is implicit thanks to POST method
 routes.get('/categories', categories.get);
 
-routes.post('/goals', goals.create); // ----DONE---TODO: make this into '/goals'. 'create' is implicit thanks to POST method
+routes.post('/goals', goals.create);
 routes.get('/goals', goals.get);
-routes.put('/goals/:id', goals.update); // ----DONE----TODO: make this into '/goals/:id'. 'update' is implicit thanks to PUT method
+routes.put('/goals/:id', goals.update);
 routes.delete('/goals/:goal_id', goals.delete);
 
 routes.get('/transactions', transactions.get);
-routes.put('/transactions', transactions.update); // ----DONE---- TODO: make this into '/transactions'. 'update' is implicit thanks to PUT method
+routes.put('/transactions', transactions.update);
 
 routes.get('/businesses', businesses.get);
 
