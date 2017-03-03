21b4fb1dd5171e699155249dba108574f45dd068
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-03 14:51:27 -0800

    style jumbotron two

--- client/components/JumbotronTwo.jsx
+++ client/components/JumbotronTwo.jsx
@@ -8,16 +8,16 @@ const JumbotronTwo = props => (
         <div className="col-md-12 bigquick">You plug in your transaction data. It's easy and we never share your data.</div>
       </div>
       <div className="row whiteback explainerdiv">
-        <div className="col-md-12 bigquick">We crunch the numbers</div>
+        <div className="col-md-12 bigquick"><h1 className="d7">Example</h1>We crunch the numbers</div>
       </div>
       <div className="row whiteback explainerdiv">
-        <div className="col-md-12 bigquick">You get lots of fancy charts </div>
+        <div className="col-md-12 bigquick"><h1 className="c">Example</h1>You get lots of fancy charts </div>
       </div>
       <div className="row whiteback explainerdiv">
-        <div className="col-md-12 bigquick">We present you with games designed to help you reduce spending</div>
+        <div className="col-md-12 bigquick"><h1 className="af">Example</h1>We present you with games designed to help you reduce spending</div>
       </div>
       <div className="row whiteback explainerdiv">
-        <div className="col-md-12 bigquick">You win at life </div>
+        <div className="col-md-12 bigquick"><h1 className="d">Example</h1>You win at life </div>
       </div>
       <h1>It's that easy</h1>
     </div>
