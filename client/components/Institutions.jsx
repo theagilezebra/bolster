95f6742a08508a5a7164c952621422f88e45a857
MarcManiez <marc.maniez@gmail.com>
2017-03-02 18:08:21 -0800

    Add error message when bank account delete fails.

--- client/components/Institutions.jsx
+++ client/components/Institutions.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { renderAccountGroups, groupAccounts } from '../helpers/accountHelpers.jsx';
 import AddBank from './AddBank.jsx';
 
-const Institutions = ({ accounts, dispatch }) => (
+const Institutions = ({ accounts, dispatch, error }) => (
   <div>
     <h2>Accounts</h2>
-    {renderAccountGroups(groupAccounts(accounts, dispatch))}
+    {renderAccountGroups(groupAccounts(accounts), dispatch)}
+    <div className="error-message">{error.deleteBankAccount}</div>
     <h3>Add a bank</h3>
     <AddBank />
   </div>
