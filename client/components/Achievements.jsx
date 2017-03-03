f535e4cd2ebe08b9e7d68bc7806247f6658db3d7
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-02 17:21:01 -0800

    remove game compnents, call function to display games

--- client/components/Achievements.jsx
+++ client/components/Achievements.jsx
@@ -2,27 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Game from './Game.jsx';
 import NavigationBar from './NavBar.jsx';
-import Stats from './Stats.jsx';
-// import { getPurchases, progressBar, average, previousMonth } from '../helpers/gameHelpers';
+import { displayAchievements } from '../helpers/achievementHelpers.js';
 
 const Achievements = props => (
   <div>
     <NavigationBar />
     <div className="container quicksand">
       <div className="row">
-        <Game name="Trappist Monk" start="2017-01-29" days={`${1}`} period="daily" />
-        <Game name="Daily Hero" start="2015-10-20" days={`${7}`} period="daily" />
-        <Game name="Weekly Hero" start="2015-10-20" days={`${7}`} period="weekly" />
+
+        {
+           displayAchievements(props.achievements)
+        }
       </div>
-    </div>
-    <div >
-      <h1 className="quicksand centertext" >Past Achievements</h1>
-      <Stats />
     </div>
   </div>
   );
 
 export default connect(state => ({
-  transactions: state.transactions.transactionsData,
+  achievements: state.achievements.achievementsData,
 }))(Achievements);
 
