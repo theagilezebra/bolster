eb72d1cfd24aff8401cf71e8f124f5082327463a
MarcManiez <marc.maniez@gmail.com>
2017-03-03 16:22:49 -0800

    Store purchase types object in separate file.

---
+++ database/sampleData/purchaseTypes.js
@@ -0,0 +1,86 @@
+module.exports = {
+  drinkingOut: {
+    _account: '0123456789',
+    category_id: '13001001',
+    name: 'Nectar Wine Lounge',
+    meta: {
+      location: {
+        address: '3330 Steiner St',
+        city: 'San Francisco',
+        state: 'CA',
+      },
+    },
+  },
+  eatingOut: {
+    _account: '0123456789',
+    category_id: '13005029',
+    name: 'Senior Sisig',
+    meta: {
+      location: {
+        address: '300 Pine St',
+        city: 'San Francisco',
+        state: 'CA',
+      },
+    },
+  },
+  entertainment: {
+    _account: '0123456789',
+    category_id: '17001009',
+    name: 'AMC Metreon 16',
+    meta: {
+      location: {
+        address: '135 4th St #3000',
+        city: 'San Francisco',
+        state: 'CA',
+      },
+    },
+  },
+  utilities: {
+    _account: '0123456789',
+    category_id: '18068000',
+    name: 'PG&E',
+    meta: {
+      location: {
+        address: '77 Beale St',
+        city: 'San Francisco',
+        state: 'CA',
+      },
+    },
+  },
+  clothesShopping: {
+    _account: '0123456789',
+    category_id: '19012001',
+    name: 'Nordstrom',
+    meta: {
+      location: {
+        address: '865 Market St',
+        city: 'San Francisco',
+        state: 'CA',
+      },
+    },
+  },
+  foodShopping: {
+    _account: '0123456789',
+    category_id: '19025002',
+    name: 'Whole Foods',
+    meta: {
+      location: {
+        address: '2001 Market St',
+        city: 'San Francisco',
+        state: 'CA',
+      },
+    },
+  },
+  transportation: {
+    _account: '0123456789',
+    category_id: '22016000',
+    name: 'Uber',
+    meta: {
+      location: {
+        address: '1455 Market St #400',
+        city: 'San Francisco',
+        state: 'CA',
+      },
+    },
+  },
+};
