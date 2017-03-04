c44bd176a8b387937ee459fd44c43c394dc5b7ad
MarcManiez <marc.maniez@gmail.com>
2017-03-03 20:33:01 -0800

    Delete accounts & transactions recreate them.

--- database/sampleData/populatePerfectUser.js
+++ database/sampleData/populatePerfectUser.js
@@ -1,8 +1,11 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt-nodejs');
 const Account = require('../models/account');
+const Achievement = require('../models/achievement');
+const transactionsController = require('../../server/controllers/transactions');
 
 const helpers = require('../../server/helpers');
+const perfectUser = require('./perfectUser');
 
 const user = {
   email: 'george@costanza.com',
@@ -14,13 +17,15 @@ const user = {
 };
 const { email, password, firstName, lastName } = user;
 
-// find the user
-// if he exists
-  // cool, grab his id
-// else
-  // create it
-// delete all his accounts and achievements
-// populate the database
+const accountObject = {
+  institutionName: 'Bank of America',
+  accountType: 'checking account',
+  plaidAccountId: '0123456789',
+  availableBalance: 10000,
+  currentBalance: 10000,
+  name: 'my sole account',
+};
+
 let userId;
 
 const encrypt = pass => new Promise((resolve, reject) => {
@@ -36,30 +41,22 @@ User.forge({ email }).fetch()
   User.forge(user).save();
 }))
 .then((userInstance) => {
-  userId = user.id;
-  return userInstance;
-});
-  // if (userInstance) {
-  //   userId = userInstance.id;
-  //   return userInstance;
-  // }
-  // bcrypt.hash((password), null, null, (err, hash) => {
-  //   user.password = hash;
-  //   return User.forge(user).save();
-  // });
+  userId = userInstance.id;
+  return Account.forge({ institutionName: 'Bank of America', user_id: user.id }).fetchAll({ withRelated: ['transactions'] });
+})
+.then(accounts => Promise.all(accounts.models.map(account => Promise.all(account.relations.transactions.map(transaction => transaction.destroy()))))
+  .then(() => Promise.all(accounts.map(account => account.destroy()))))
+.then(() => User.forge({ id: userId }).save({ publicToken: null, accessToken: null }))
+.then(() => Achievement.forge({ user_id: userId }).fetchAll())
+.then(achievements => Promise.all(achievements.map(achievement => achievement.destroy())))
+.then(() => {
+  accountObject.user_id = userId;
+  return Account.forge(accountObject).save();
+})
+.then(() => {
+  const transactions = perfectUser({ startDate: new Date(2016, 11, 30), endDate: new Date() });
+  return transactionsController.bulkCreate(transactions, userId);
+})
+.catch(err => console.log(err));
 
-  // return user || 'create user promise';
-
-
-// find or create the user in database
-
-
-// find or create a bank account
-// institutionName: Bank of America
-// accountType: checking account
-// plaidAccountId: 0123456789
-// availableBalance: 1000000
-// currentBalance: 1000000
-// user_id: to be provided after having found / created George Costanza
-
-// bulk create the transactions
+// TODO: write some middleware for this function and run it every time someone hits index.html
