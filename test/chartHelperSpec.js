4cf378a81c9f1f0e4f60bdd32b54e0e6142608ac
colbisaurusrex <c.barthelmess1@gmail.com>
2017-03-04 16:49:04 -0800

    reformat tests for better readability

--- test/chartHelperSpec.js
+++ test/chartHelperSpec.js
@@ -1,48 +1,50 @@
 const { expect, assert } = require('chai');
 const { sortByDate, dateLabels } = require('../client/helpers/chartHelpers.jsx');
 
-describe('sortByDate helper function', () => {
-  it('sortByDate should sort transactions by date', () => {
-    const transactions = [
-     { amount: 100, date: '2017-02-24' },
-     { amount: 56, date: '2017-02-02' },
-     { amount: 32, date: '2017-02-27' },
-     { amount: 100, date: '2017-02-22' },
-    ];
-    assert.equal(transactions.sort(sortByDate)[0].date, '2017-02-02');
-    assert.equal(transactions.sort(sortByDate)[1].date, '2017-02-22');
-    assert.equal(transactions.sort(sortByDate)[2].date, '2017-02-24');
-    assert.equal(transactions.sort(sortByDate)[3].date, '2017-02-27');
+describe('Chart Helpers', () => {
+  describe('sortByDate helper function', () => {
+    it('sortByDate should sort transactions by date', () => {
+      const transactions = [
+      { amount: 100, date: '2017-02-24' },
+      { amount: 56, date: '2017-02-02' },
+      { amount: 32, date: '2017-02-27' },
+      { amount: 100, date: '2017-02-22' },
+      ];
+      assert.equal(transactions.sort(sortByDate)[0].date, '2017-02-02');
+      assert.equal(transactions.sort(sortByDate)[1].date, '2017-02-22');
+      assert.equal(transactions.sort(sortByDate)[2].date, '2017-02-24');
+      assert.equal(transactions.sort(sortByDate)[3].date, '2017-02-27');
+    });
   });
-});
 
-describe('dateLabels helper function', () => {
-  const unformattedLabels = {
-    '2017-02-24': 100,
-    '2017-02-02': 56,
-    '2017-02-27': 32,
-    '2017-02-22': 100,
-  };
-  const variedFormats = {
-    '02-24-2017': 100,
-    '02/02/2017': 56,
-    '2017-02-27': 32,
-    '2017-02-22': 100,
-  };
-  it('should reformat dates to Month, Day Year', () => {
-    const formattedLabels = dateLabels(unformattedLabels);
-    expect(unformattedLabels).to.be.a('object');
-    assert.equal(formattedLabels[0], 'February 24th 2017');
-    assert.equal(formattedLabels[1], 'February 2nd 2017');
-    assert.equal(formattedLabels[2], 'February 27th 2017');
-    assert.equal(formattedLabels[3], 'February 22nd 2017');
-  });
-  it('should reformat different format inputs', () => {
-    const formattedLabels = dateLabels(variedFormats);
-    expect(variedFormats).to.be.a('object');
-    assert.equal(formattedLabels[0], 'February 24th 2017');
-    assert.equal(formattedLabels[1], 'February 2nd 2017');
-    assert.equal(formattedLabels[2], 'February 27th 2017');
-    assert.equal(formattedLabels[3], 'February 22nd 2017');
+  describe('dateLabels helper function', () => {
+    const unformattedLabels = {
+      '2017-02-24': 100,
+      '2017-02-02': 56,
+      '2017-02-27': 32,
+      '2017-02-22': 100,
+    };
+    const variedFormats = {
+      '02-24-2017': 100,
+      '02/02/2017': 56,
+      '2017-02-27': 32,
+      '2017-02-22': 100,
+    };
+    it('should reformat dates to Month, Day Year', () => {
+      const formattedLabels = dateLabels(unformattedLabels);
+      expect(unformattedLabels).to.be.a('object');
+      assert.equal(formattedLabels[0], 'February 24th 2017');
+      assert.equal(formattedLabels[1], 'February 2nd 2017');
+      assert.equal(formattedLabels[2], 'February 27th 2017');
+      assert.equal(formattedLabels[3], 'February 22nd 2017');
+    });
+    it('should reformat different format inputs', () => {
+      const formattedLabels = dateLabels(variedFormats);
+      expect(variedFormats).to.be.a('object');
+      assert.equal(formattedLabels[0], 'February 24th 2017');
+      assert.equal(formattedLabels[1], 'February 2nd 2017');
+      assert.equal(formattedLabels[2], 'February 27th 2017');
+      assert.equal(formattedLabels[3], 'February 22nd 2017');
+    });
   });
 });
--- test/transactionHelpersSpec.js
+++ test/transactionHelpersSpec.js
@@ -7,49 +7,51 @@ const sampleData = [
   { account_id: 1, amount: 3.45, categories: ['Interest'], date: '02/25/2017', id: 4, name: 'Savings interest', user_id: 1 },
 ];
 
-describe('mapCategories transaction helper functions', () => {
-  const mappedTransactions = mapCategories(sampleData);
-  it('should produce an object', () => {
-    expect(mappedTransactions).to.be.a('object');
+describe('Transaction Helpers', () => {
+  describe('mapCategories transaction helper functions', () => {
+    const mappedTransactions = mapCategories(sampleData);
+    it('should produce an object', () => {
+      expect(mappedTransactions).to.be.a('object');
+    });
+    it('should ignore Transfer and Interest categories', () => {
+      expect(Object.keys(mappedTransactions).length).to.equal(2);
+      expect(mappedTransactions).to.not.have.any.keys('Interest', 'Transfer');
+    });
+    it('should use the first item in the categories array as the key', () => {
+      expect(Object.keys(mappedTransactions)[0]).to.equal(sampleData[0].categories[0]);
+      expect(Object.keys(mappedTransactions)[1]).to.equal(sampleData[2].categories[0]);
+    });
+    it('should pair item amounts as values for each date key', () => {
+      expect(mappedTransactions[sampleData[0].categories[0]]).to.equal(sampleData[0].amount);
+      expect(mappedTransactions[sampleData[2].categories[0]]).to.equal(sampleData[2].amount);
+    });
   });
-  it('should ignore Transfer and Interest categories', () => {
-    expect(Object.keys(mappedTransactions).length).to.equal(2);
-    expect(mappedTransactions).to.not.have.any.keys('Interest', 'Transfer');
-  });
-  it('should use the first item in the categories array as the key', () => {
-    expect(Object.keys(mappedTransactions)[0]).to.equal(sampleData[0].categories[0]);
-    expect(Object.keys(mappedTransactions)[1]).to.equal(sampleData[2].categories[0]);
-  });
-  it('should pair item amounts as values for each date key', () => {
-    expect(mappedTransactions[sampleData[0].categories[0]]).to.equal(sampleData[0].amount);
-    expect(mappedTransactions[sampleData[2].categories[0]]).to.equal(sampleData[2].amount);
-  });
-});
 
-describe('labelize transaction helper function', () => {
-  const labelizedTransactions = labelize(sampleData);
-  it('should produce and array', () => {
-    expect(labelizedTransactions).to.be.a('array');
+  describe('labelize transaction helper function', () => {
+    const labelizedTransactions = labelize(sampleData);
+    it('should produce and array', () => {
+      expect(labelizedTransactions).to.be.a('array');
+    });
+    it('should ignore Transfer and Interest categories', () => {
+      expect(labelizedTransactions).not.include('Transfers', 'Interest');
+    });
+    it('should use the first item in the transaction categories array to produce array', () => {
+      expect(labelizedTransactions[0]).to.equal('Food and Drink');
+      expect(labelizedTransactions[1]).to.equal('Shops');
+    });
   });
-  it('should ignore Transfer and Interest categories', () => {
-    expect(labelizedTransactions).not.include('Transfers', 'Interest');
-  });
-  it('should use the first item in the transaction categories array to produce array', () => {
-    expect(labelizedTransactions[0]).to.equal('Food and Drink');
-    expect(labelizedTransactions[1]).to.equal('Shops');
-  });
-});
 
-describe('populateChart transaction helper function', () => {
-  const chartConfiguration = populateChart(sampleData);
-  it('should produce labels for a chart', () => {
-    expect(chartConfiguration.labels).to.be.a('array');
-    expect(chartConfiguration.labels[0]).to.equal('Food and Drink');
-    expect(chartConfiguration.labels[1]).to.equal('Shops');
-  });
-  it('should produce a an array of data from transaction amounts', () => {
-    expect(chartConfiguration.datasets[0].data[0]).to.equal(34.23);
-    expect(chartConfiguration.datasets[0].data[1]).to.equal(54.23);
+  describe('populateChart transaction helper function', () => {
+    const chartConfiguration = populateChart(sampleData);
+    it('should produce labels for a chart', () => {
+      expect(chartConfiguration.labels).to.be.a('array');
+      expect(chartConfiguration.labels[0]).to.equal('Food and Drink');
+      expect(chartConfiguration.labels[1]).to.equal('Shops');
+    });
+    it('should produce a an array of data from transaction amounts', () => {
+      expect(chartConfiguration.datasets[0].data[0]).to.equal(34.23);
+      expect(chartConfiguration.datasets[0].data[1]).to.equal(54.23);
+    });
   });
 });
 
